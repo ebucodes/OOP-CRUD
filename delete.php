1475fd88b0825ba8ccc3b81ca4a20015d80d8abf
EbuCodes <ebucodes@gmail.com>
2022-05-19 14:54:35 +0100

    final changes to Delete

--- delete.php
+++ delete.php
@@ -1,29 +1,22 @@
 <?php
-
-include("model.php");
-
-$model = new Model();
-
-$id = $_REQUEST['id'];
-$delete = $model->delete($id);
-
-if ($delete) {
-?>
-    <script>
-        alert("User deleted successfully")
-    </script>
-    <script>
-        window.location.href = "records.php";
-    </script>
-<?php
-} else {
-?>
-    <script>
-        alert("User not deleted")
-    </script>
-    <script>
-        window.location.href = "records.php";
-    </script>
-<?php
+session_start();
+if (strlen($_SESSION['userID']) == "") {
+    header('location:logout.php');
 }
 ?>
+<?php
+
+include("classes/model.php");
+
+$user = new User();
+
+$id = $_REQUEST['id'];
+$delete = $user->delete($id);
+
+if ($delete) {
+    echo '<script>alert("User deleted successfully")</script>';
+    echo '<script>window.location.href = "records.php";</script>';
+} else {
+    echo '<script>alert("User not deleted")</script>';
+    echo '<script>window.location.href = "records.php";</script>';
+}
